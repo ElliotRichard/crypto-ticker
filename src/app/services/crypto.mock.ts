48b2ae6e882cf8a383f1c688ec77dabdfbfb2079
DickElliot <elliotrichard@protonmail.com>
2021-10-25 23:54:34 +1300

    Added service tests

---
+++ src/app/services/crypto.mock.ts
@@ -0,0 +1,2 @@
+export const MockCryptoList: string[] = ['BTCUSDT'];
+export const MockCryptoResponse: any[] = [{ symbol: 'BTCUSDT' }];
--- src/app/services/crypto.service.spec.ts
+++ src/app/services/crypto.service.spec.ts
@@ -1,20 +1,42 @@
-import { TestBed } from '@angular/core/testing';
-
+import { TestBed, inject } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { CryptoService } from './crypto.service';
+import { MockCryptoList, MockCryptoResponse } from './crypto.mock';
 
 describe('CryptoService', () => {
+  const baseUrl = 'https://api2.binance.com/api/v3';
   let service: CryptoService;
+  let http: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [CryptoService],
+    });
     service = TestBed.inject(CryptoService);
+    http = TestBed.inject(HttpTestingController);
+  });
+  afterEach(() => {
+    http.verify();
   });
 
-  it('should be created', () => {
+  it('should instantiate', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should have a crypto property', () => {
+  it('should manage a list of cryptos', () => {
     expect(service.get()).toContain('BTCUSDT');
+    service.add('TEST');
+    expect(service.get()).toEqual([...MockCryptoList, 'TEST']);
+  });
+
+  it('should load crypto data from API', (done) => {
+    service.load(MockCryptoList).subscribe((result) => {
+      expect(result).toEqual(MockCryptoResponse);
+      done();
+    });
   });
 });
